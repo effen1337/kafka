4a1bff857dbb830ebf280741eb16f21a3ac7b6d9
John Roesler <vvcephei@apache.org>
2021-12-20 12:24:51 -0600

    MINOR: add PAPI KV store tests for IQv2

--- streams/src/test/java/org/apache/kafka/streams/integration/IQv2StoreIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/IQv2StoreIntegrationTest.java
@@ -35,6 +35,9 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.SessionWindows;
 import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.processor.api.ContextualProcessor;
+import org.apache.kafka.streams.processor.api.ProcessorSupplier;
+import org.apache.kafka.streams.processor.api.Record;
 import org.apache.kafka.streams.query.FailureReason;
 import org.apache.kafka.streams.query.KeyQuery;
 import org.apache.kafka.streams.query.Position;
@@ -49,8 +52,10 @@ import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.SessionBytesStoreSupplier;
 import org.apache.kafka.streams.state.SessionStore;
+import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.StoreSupplier;
 import org.apache.kafka.streams.state.Stores;
+import org.apache.kafka.streams.state.TimestampedKeyValueStore;
 import org.apache.kafka.streams.state.ValueAndTimestamp;
 import org.apache.kafka.streams.state.WindowBytesStoreSupplier;
 import org.apache.kafka.streams.state.WindowStore;
@@ -58,12 +63,15 @@ import org.apache.kafka.test.IntegrationTest;
 import org.apache.kafka.test.TestUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.AssumptionViolatedException;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -75,8 +83,10 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -100,6 +110,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @RunWith(value = Parameterized.class)
 public class IQv2StoreIntegrationTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(IQv2StoreIntegrationTest.class);
+
+    private static final long SEED = new Random().nextLong();
+    private static final Random RANDOM = new Random(SEED);
+
     private static final int NUM_BROKERS = 1;
     public static final Duration WINDOW_SIZE = Duration.ofMinutes(5);
     private static int port = 0;
@@ -109,6 +124,7 @@ public class IQv2StoreIntegrationTest {
 
     public static final EmbeddedKafkaCluster CLUSTER = new EmbeddedKafkaCluster(NUM_BROKERS);
     private final StoresToTest storeToTest;
+    private final String kind;
 
     public static class UnknownQuery implements Query<Void> {
 
@@ -242,21 +258,24 @@ public class IQv2StoreIntegrationTest {
             @Override
             public StoreSupplier<?> supplier() {
                 return Stores.inMemoryWindowStore(STORE_NAME, Duration.ofDays(1), WINDOW_SIZE,
-                    false);
+                                                  false
+                );
             }
         },
         ROCKS_WINDOW {
             @Override
             public StoreSupplier<?> supplier() {
                 return Stores.persistentWindowStore(STORE_NAME, Duration.ofDays(1), WINDOW_SIZE,
-                    false);
+                                                    false
+                );
             }
         },
         TIME_ROCKS_WINDOW {
             @Override
             public StoreSupplier<?> supplier() {
                 return Stores.persistentTimestampedWindowStore(STORE_NAME, Duration.ofDays(1),
-                    WINDOW_SIZE, false);
+                                                               WINDOW_SIZE, false
+                );
             }
         },
         IN_MEMORY_SESSION {
@@ -287,31 +306,42 @@ public class IQv2StoreIntegrationTest {
         }
     }
 
-    @Parameterized.Parameters(name = "cache={0}, log={1}, supplier={2}")
+    @Parameterized.Parameters(name = "cache={0}, log={1}, supplier={2}, kind={3}")
     public static Collection<Object[]> data() {
+        LOG.info("Generating test cases according to random seed: {}", SEED);
         final List<Object[]> values = new ArrayList<>();
         for (final boolean cacheEnabled : Arrays.asList(true, false)) {
             for (final boolean logEnabled : Arrays.asList(true, false)) {
                 for (final StoresToTest toTest : StoresToTest.values()) {
-                    values.add(new Object[]{cacheEnabled, logEnabled, toTest.name()});
+                    for (final String kind : Arrays.asList("DSL", "PAPI")) {
+                        values.add(new Object[]{cacheEnabled, logEnabled, toTest.name(), kind});
+                    }
                 }
             }
         }
+        // Randomizing the test cases in case some orderings interfere with each other.
+        // If you wish to reproduce a randomized order, copy the logged SEED and substitute
+        // it for the constant at the top of the file. This will cause exactly the same sequence
+        // of pseudorandom values to be generated.
+        Collections.shuffle(values, RANDOM);
         return values;
     }
 
     public IQv2StoreIntegrationTest(
         final boolean cache,
         final boolean log,
-        final String storeToTest) {
+        final String storeToTest,
+        final String kind) {
         this.cache = cache;
         this.log = log;
         this.storeToTest = StoresToTest.valueOf(storeToTest);
+        this.kind = kind;
     }
 
     @BeforeClass
     public static void before()
         throws InterruptedException, IOException, ExecutionException, TimeoutException {
+
         CLUSTER.start();
         CLUSTER.deleteAllTopicsAndWait(60 * 1000L);
         final int partitions = 2;
@@ -365,90 +395,23 @@ public class IQv2StoreIntegrationTest {
         final Properties streamsConfig = streamsConfiguration(
             cache,
             log,
-            storeToTest.name()
+            storeToTest.name(),
+            kind
         );
 
         final StreamsBuilder builder = new StreamsBuilder();
-        if (supplier instanceof KeyValueBytesStoreSupplier) {
-            final Materialized<Integer, Integer, KeyValueStore<Bytes, byte[]>> materialized =
-                Materialized.as((KeyValueBytesStoreSupplier) supplier);
-
-            if (cache) {
-                materialized.withCachingEnabled();
-            } else {
-                materialized.withCachingDisabled();
-            }
-
-            if (log) {
-                materialized.withLoggingEnabled(Collections.emptyMap());
-            } else {
-                materialized.withCachingDisabled();
-            }
-
-            if (storeToTest.global()) {
-                builder.globalTable(
-                    INPUT_TOPIC_NAME,
-                    Consumed.with(Serdes.Integer(), Serdes.Integer()),
-                    materialized
-                );
-            } else {
-                builder.table(
-                    INPUT_TOPIC_NAME,
-                    Consumed.with(Serdes.Integer(), Serdes.Integer()),
-                    materialized
-                );
-            }
-        } else if (supplier instanceof WindowBytesStoreSupplier) {
-            final Materialized<Integer, Integer, WindowStore<Bytes, byte[]>> materialized =
-                Materialized.as((WindowBytesStoreSupplier) supplier);
-
-            if (cache) {
-                materialized.withCachingEnabled();
-            } else {
-                materialized.withCachingDisabled();
-            }
-
-            if (log) {
-                materialized.withLoggingEnabled(Collections.emptyMap());
-            } else {
-                materialized.withCachingDisabled();
-            }
-
-            builder
-                .stream(INPUT_TOPIC_NAME, Consumed.with(Serdes.Integer(), Serdes.Integer()))
-                .groupByKey()
-                .windowedBy(TimeWindows.ofSizeWithNoGrace(WINDOW_SIZE))
-                .aggregate(
-                    () -> 0,
-                    (key, value, aggregate) -> aggregate + value,
-                    materialized
-                );
-        } else if (supplier instanceof SessionBytesStoreSupplier) {
-            final Materialized<Integer, Integer, SessionStore<Bytes, byte[]>> materialized =
-                Materialized.as((SessionBytesStoreSupplier) supplier);
-
-            if (cache) {
-                materialized.withCachingEnabled();
-            } else {
-                materialized.withCachingDisabled();
-            }
-
-            if (log) {
-                materialized.withLoggingEnabled(Collections.emptyMap());
-            } else {
-                materialized.withCachingDisabled();
-            }
-
-            builder
-                .stream(INPUT_TOPIC_NAME, Consumed.with(Serdes.Integer(), Serdes.Integer()))
-                .groupByKey()
-                .windowedBy(SessionWindows.ofInactivityGapWithNoGrace(WINDOW_SIZE))
-                .aggregate(
-                    () -> 0,
-                    (key, value, aggregate) -> aggregate + value,
-                    (aggKey, aggOne, aggTwo) -> aggOne + aggTwo,
-                    materialized
-                );
+        if (Objects.equals(kind, "DSL") && supplier instanceof KeyValueBytesStoreSupplier) {
+            setUpKeyValueDSLTopology((KeyValueBytesStoreSupplier) supplier, builder);
+        } else if (Objects.equals(kind, "PAPI") && supplier instanceof KeyValueBytesStoreSupplier) {
+            setUpKeyValuePAPITopology((KeyValueBytesStoreSupplier) supplier, builder);
+        } else if (Objects.equals(kind, "DSL") && supplier instanceof WindowBytesStoreSupplier) {
+            setUpWindowDSLTopology((WindowBytesStoreSupplier) supplier, builder);
+        } else if (Objects.equals(kind, "PAPI") && supplier instanceof WindowBytesStoreSupplier) {
+            throw new AssumptionViolatedException("Case not implemented yet");
+        } else if (Objects.equals(kind, "DSL") && supplier instanceof SessionBytesStoreSupplier) {
+            setUpSessionDSLTopology((SessionBytesStoreSupplier) supplier, builder);
+        } else if (Objects.equals(kind, "PAPI") && supplier instanceof SessionBytesStoreSupplier) {
+            throw new AssumptionViolatedException("Case not implemented yet");
         } else {
             throw new AssertionError("Store supplier is an unrecognized type.");
         }
@@ -464,10 +427,168 @@ public class IQv2StoreIntegrationTest {
             );
     }
 
+    private void setUpSessionDSLTopology(final SessionBytesStoreSupplier supplier,
+                                         final StreamsBuilder builder) {
+        final Materialized<Integer, Integer, SessionStore<Bytes, byte[]>> materialized =
+            Materialized.as(supplier);
+
+        if (cache) {
+            materialized.withCachingEnabled();
+        } else {
+            materialized.withCachingDisabled();
+        }
+
+        if (log) {
+            materialized.withLoggingEnabled(Collections.emptyMap());
+        } else {
+            materialized.withCachingDisabled();
+        }
+
+        builder
+            .stream(INPUT_TOPIC_NAME, Consumed.with(Serdes.Integer(), Serdes.Integer()))
+            .groupByKey()
+            .windowedBy(SessionWindows.ofInactivityGapWithNoGrace(WINDOW_SIZE))
+            .aggregate(
+                () -> 0,
+                (key, value, aggregate) -> aggregate + value,
+                (aggKey, aggOne, aggTwo) -> aggOne + aggTwo,
+                materialized
+            );
+    }
+
+    private void setUpWindowDSLTopology(final WindowBytesStoreSupplier supplier,
+                                        final StreamsBuilder builder) {
+        final Materialized<Integer, Integer, WindowStore<Bytes, byte[]>> materialized =
+            Materialized.as(supplier);
+
+        if (cache) {
+            materialized.withCachingEnabled();
+        } else {
+            materialized.withCachingDisabled();
+        }
+
+        if (log) {
+            materialized.withLoggingEnabled(Collections.emptyMap());
+        } else {
+            materialized.withCachingDisabled();
+        }
+
+        builder
+            .stream(INPUT_TOPIC_NAME, Consumed.with(Serdes.Integer(), Serdes.Integer()))
+            .groupByKey()
+            .windowedBy(TimeWindows.ofSizeWithNoGrace(WINDOW_SIZE))
+            .aggregate(
+                () -> 0,
+                (key, value, aggregate) -> aggregate + value,
+                materialized
+            );
+    }
+
+    private void setUpKeyValueDSLTopology(final KeyValueBytesStoreSupplier supplier,
+                                          final StreamsBuilder builder) {
+        final Materialized<Integer, Integer, KeyValueStore<Bytes, byte[]>> materialized =
+            Materialized.as(supplier);
+
+        if (cache) {
+            materialized.withCachingEnabled();
+        } else {
+            materialized.withCachingDisabled();
+        }
+
+        if (log) {
+            materialized.withLoggingEnabled(Collections.emptyMap());
+        } else {
+            materialized.withCachingDisabled();
+        }
+
+        if (storeToTest.global()) {
+            builder.globalTable(
+                INPUT_TOPIC_NAME,
+                Consumed.with(Serdes.Integer(), Serdes.Integer()),
+                materialized
+            );
+        } else {
+            builder.table(
+                INPUT_TOPIC_NAME,
+                Consumed.with(Serdes.Integer(), Serdes.Integer()),
+                materialized
+            );
+        }
+    }
+
+    private void setUpKeyValuePAPITopology(final KeyValueBytesStoreSupplier supplier,
+                                           final StreamsBuilder builder) {
+        final StoreBuilder<?> keyValueStoreStoreBuilder;
+        final ProcessorSupplier<Integer, Integer, Void, Void> processorSupplier;
+        if (storeToTest.timestamped()) {
+            keyValueStoreStoreBuilder = Stores.timestampedKeyValueStoreBuilder(
+                supplier,
+                Serdes.Integer(),
+                Serdes.Integer()
+            );
+            processorSupplier = () -> new ContextualProcessor<Integer, Integer, Void, Void>() {
+                @Override
+                public void process(final Record<Integer, Integer> record) {
+                    final TimestampedKeyValueStore<Integer, Integer> stateStore =
+                        context().getStateStore(keyValueStoreStoreBuilder.name());
+                    stateStore.put(
+                        record.key(),
+                        ValueAndTimestamp.make(
+                            record.value(), record.timestamp()
+                        )
+                    );
+                }
+            };
+        } else {
+            keyValueStoreStoreBuilder = Stores.keyValueStoreBuilder(
+                supplier,
+                Serdes.Integer(),
+                Serdes.Integer()
+            );
+            processorSupplier =
+                () -> new ContextualProcessor<Integer, Integer, Void, Void>() {
+                    @Override
+                    public void process(final Record<Integer, Integer> record) {
+                        final KeyValueStore<Integer, Integer> stateStore =
+                            context().getStateStore(keyValueStoreStoreBuilder.name());
+                        stateStore.put(record.key(), record.value());
+                    }
+                };
+        }
+        if (cache) {
+            keyValueStoreStoreBuilder.withCachingEnabled();
+        } else {
+            keyValueStoreStoreBuilder.withCachingDisabled();
+        }
+        if (log) {
+            keyValueStoreStoreBuilder.withLoggingEnabled(Collections.emptyMap());
+        } else {
+            keyValueStoreStoreBuilder.withCachingDisabled();
+        }
+        if (storeToTest.global()) {
+            builder.addGlobalStore(
+                keyValueStoreStoreBuilder,
+                INPUT_TOPIC_NAME,
+                Consumed.with(Serdes.Integer(), Serdes.Integer()),
+                processorSupplier
+            );
+        } else {
+            builder.addStateStore(keyValueStoreStoreBuilder);
+            builder
+                .stream(INPUT_TOPIC_NAME, Consumed.with(Serdes.Integer(), Serdes.Integer()))
+                .process(processorSupplier, keyValueStoreStoreBuilder.name());
+        }
+
+    }
+
+
     @After
     public void afterTest() {
-        kafkaStreams.close();
-        kafkaStreams.cleanUp();
+        // only needed because some of the PAPI cases aren't added yet.
+        if (kafkaStreams != null) {
+            kafkaStreams.close();
+            kafkaStreams.cleanUp();
+        }
     }
 
     @AfterClass
@@ -542,11 +663,15 @@ public class IQv2StoreIntegrationTest {
         final StateQueryResult<ValueAndTimestamp<Integer>> result = kafkaStreams.query(request);
 
         assertThat(result.getGlobalResult().isFailure(), is(true));
-        assertThat(result.getGlobalResult().getFailureReason(),
-            is(FailureReason.UNKNOWN_QUERY_TYPE));
-        assertThat(result.getGlobalResult().getFailureMessage(),
+        assertThat(
+            result.getGlobalResult().getFailureReason(),
+            is(FailureReason.UNKNOWN_QUERY_TYPE)
+        );
+        assertThat(
+            result.getGlobalResult().getFailureMessage(),
             is("Global stores do not yet support the KafkaStreams#query API."
-                + " Use KafkaStreams#store instead."));
+                   + " Use KafkaStreams#store instead.")
+        );
     }
 
     public void shouldRejectUnknownQuery() {
@@ -564,9 +689,12 @@ public class IQv2StoreIntegrationTest {
             queryResult -> {
                 assertThat(queryResult.isFailure(), is(true));
                 assertThat(queryResult.isSuccess(), is(false));
-                assertThat(queryResult.getFailureReason(),
-                    is(FailureReason.UNKNOWN_QUERY_TYPE));
-                assertThat(queryResult.getFailureMessage(),
+                assertThat(
+                    queryResult.getFailureReason(),
+                    is(FailureReason.UNKNOWN_QUERY_TYPE)
+                );
+                assertThat(
+                    queryResult.getFailureMessage(),
                     matchesPattern(
                         "This store (.*)"
                             + " doesn't know how to execute the given query"
@@ -737,9 +865,10 @@ public class IQv2StoreIntegrationTest {
     }
 
     private static Properties streamsConfiguration(final boolean cache, final boolean log,
-        final String supplier) {
+                                                   final String supplier, final String kind) {
         final String safeTestName =
-            IQv2StoreIntegrationTest.class.getName() + "-" + cache + "-" + log + "-" + supplier;
+            IQv2StoreIntegrationTest.class.getName() + "-" + cache + "-" + log + "-" + supplier
+                + "-" + kind;
         final Properties config = new Properties();
         config.put(StreamsConfig.TOPOLOGY_OPTIMIZATION_CONFIG, StreamsConfig.OPTIMIZE);
         config.put(StreamsConfig.APPLICATION_ID_CONFIG, "app-" + safeTestName);
